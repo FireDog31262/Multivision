7856bef01d08440e480ab5d8f96389f9889a44da
Jerry Sparkman <jerry.sparkman@gmail.com>
2015-05-06 19:20:27 -0400

    Test Refactoring

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ var express = require('express');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
-
 var config = require('./server/config/config')[env];
 
+//Configurattion
 require('./server/config/express')(app, config);
-
 require('./server/config/mongoose')(config);
-
 require('./server/config/passport')();
-
 require('./server/config/routes')(app);
 
 var server = http.createServer(app);
